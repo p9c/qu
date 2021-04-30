661755ea4f484d51e51ad787499cfcab0f51b961
Loki Verloren <stalker.loki@protonmail.ch>
2021-04-30 18:18:54 +0200

    automating the automation

--- version/version.go
+++ version/version.go
@@ -11,14 +11,14 @@ var (
 	// URL is the git URL for the repository
 	URL = "github.com/p9c/qu"
 	// GitRef is the gitref, as in refs/heads/branchname
-	GitRef = "refs/heads/main"
+	GitRef = "HEAD"
 	// GitCommit is the commit hash of the current HEAD
 	GitCommit = "621dafa30f1e2b6f8ca804f55b65fb7d80a7a4e3"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2021-04-30T17:28:25+02:00"
+	BuildTime = "2021-04-30T18:12:29+02:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.0.6"
+	Tag = "v0.0.7"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/loki/src/github.com/p9c/pod/pkg/qu/"
 	// Major is the major number from the tag
@@ -26,7 +26,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 6
+	Patch = 7
 	// Meta is the extra arbitrary string field from Semver spec
 	Meta = ""
 )
