af12c4c484aa8e499fcd230bfc612b97ac502e99
Loki Verloren <stalker.loki@protonmail.ch>
2021-04-30 20:27:29 +0200

    foom

--- version/version.go
+++ version/version.go
@@ -11,14 +11,14 @@ var (
 	// URL is the git URL for the repository
 	URL = "github.com/p9c/qu"
 	// GitRef is the gitref, as in refs/heads/branchname
-	GitRef = "HEAD"
+	GitRef = "refs/heads/main"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "5d9c8a3582acc8a56b7871a5f33950d3623e08b6"
+	GitCommit = "d6fe62c87da66de03e5c4f416e968f5e08b6f1f1"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2021-04-30T18:34:14+02:00"
+	BuildTime = "2021-04-30T20:19:18+02:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.0.9"
+	Tag = "v0.0.10"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/loki/src/github.com/p9c/pod/pkg/qu/"
 	// Major is the major number from the tag
@@ -26,7 +26,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 9
+	Patch = 10
 	// Meta is the extra arbitrary string field from Semver spec
 	Meta = ""
 )
